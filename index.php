78e8fcce802a68b835b82a2b53cc9c5e20243077
Jason ShuJi Chen <Shuji.chen1997@gmail.com>
2023-02-14 15:20:29 -0700

    Update index.php
    - Added comments

--- index.php
+++ index.php
@@ -17,24 +17,24 @@
           <div class="row g-0">
             <div class="col-lg-6">
               <div class="card-body p-md-5 mx-md-4">
-
+                <!-- Logo -->
                 <div class="text-center">
                   <img src="img/logo.png" style="width: 350px;" alt="logo">
                 </div>
 
                 <form>
                   <p>Please login to your account</p>
-
+                  <!-- Email form -->
                   <div class="form-outline mb-4">
                     <input type="email" id="email" class="form-control"
                       placeholder="E-mail" />
                   </div>
-
+                  <!-- Password form -->
                   <div class="form-outline mb-4">
                     <input type="password" id="password" class="form-control"
                      placeholder="Password"/>
                   </div>
-
+                  <!-- Login button -->
                   <div class="text-center pt-1 mb-5 pb-1">
                     <button class="btn btn-primary btn-block fa-lg gradient-custom mb-3 w-75" type="button">Log
                       in</button>
@@ -44,6 +44,7 @@
 
               </div>
             </div>
+            <!-- Gradient Panel -->
             <div class="col-lg-6 d-flex align-items-center gradient-custom">
               <div class="text-white px-3 py-4 p-md-5 mx-md-4">
                 <h4 class="text-center">Welcome to the University Calendar!</h4>
