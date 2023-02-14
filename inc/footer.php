b32aadf7c6d53b2827c5d438288cc9e433e884ab
Jason ShuJi Chen <Shuji.chen1997@gmail.com>
2023-02-14 15:16:59 -0700

    Updated index.php
    - Added mockup login page.
    - Added Bootstrap 5.0 integration
    - Added University logo file.
    - Added header.php file
    - Added footer.php file
    - Added styles.css file

---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+<footer class="py-3 border-top">
+    <p class="text-center text-muted">© 2023 University of Calgary CPSC 471</p>
+    <p class="text-center text-muted">Shuji Chen, YiTing OuYang, Aoqi Li</p>
+  </footer>
--- index.php
+++ index.php
@@ -1,3 +1,60 @@
-<?php
-    echo "If you can see this on your browser, then it probably worked.";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <title>University of Calgary Calendar Login</title>
+</head>
+<body>
+<section class="h-100 gradient-form" style="background-color: #eee;">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-xl-10">
+        <div class="card rounded-3 text-black">
+          <div class="row g-0">
+            <div class="col-lg-6">
+              <div class="card-body p-md-5 mx-md-4">
+
+                <div class="text-center">
+                  <img src="img/logo.png" style="width: 350px;" alt="logo">
+                </div>
+
+                <form>
+                  <p>Please login to your account</p>
+
+                  <div class="form-outline mb-4">
+                    <input type="email" id="email" class="form-control"
+                      placeholder="E-mail" />
+                  </div>
+
+                  <div class="form-outline mb-4">
+                    <input type="password" id="password" class="form-control"
+                     placeholder="Password"/>
+                  </div>
+
+                  <div class="text-center pt-1 mb-5 pb-1">
+                    <button class="btn btn-primary btn-block fa-lg gradient-custom mb-3 w-75" type="button">Log
+                      in</button>
+                  </div>
+
+                </form>
+
+              </div>
+            </div>
+            <div class="col-lg-6 d-flex align-items-center gradient-custom">
+              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                <h4 class="text-center">Welcome to the University Calendar!</h4>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+<?php include 'inc/footer.php'?>
+</html>
