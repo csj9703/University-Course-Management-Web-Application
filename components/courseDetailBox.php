280a3e997abf3671cb4b718826082aaaab645c18
LcyLight <88101191+LcyLight@users.noreply.github.com>
2023-03-29 11:24:21 -0600

    updates

---
+++ components/courseDetailBox.php
@@ -0,0 +1,46 @@
+<!-- TODO: Link Course List Backened -->
+<div class="card text-dark bg-light mb-3" style="margin-top: 20px;margin-right: auto; margin-left: auto;max-width: 1000px;">
+    <div class="card-header">Course Details</div>
+    <div class="card-body">
+        <div class="input-group rounded">
+            <!-- Name -->
+            <div class="input-group mt-3 mb-3">
+                <span class="input-group-text">Course Name: </span>
+            </div>
+            <!-- User ID -->
+            <div class="input-group mt-3 mb-3">
+                <span class="input-group-text">Course Description: </span>
+            </div>
+            <!-- E-mail Info -->
+            <div class="input-group mt-3 mb-3">
+                <span class="input-group-text">Course Credits: </span>
+            </div>
+            <!-- Major Info -->
+            <div class="input-group mt-3 mb-3">
+                <span class="input-group-text">Prerequisites: </span>
+            </div>
+            <!-- Minor Info -->
+            <div class="input-group mt-3 mb-3">
+                <span class="input-group-text">Antirequisites: </span>
+            </div>
+            <!-- Courses Accordion Collapse -->
+            <div class="input-group mt-3 mb-3">
+                <button class="accordion-button collapsed" data-bs-toggle="collapse" data-bs-target="#courseCollapse" aria-expanded="false" aria-controls="antiReqCollapse">My Courses</button>
+            </div>
+            <div class="card card-body collapse" aria-expanded="false" id="courseCollapse">
+                <!-- Semester Header -->
+                <?php for ($i = 0; $i < sizeof($semArr); $i++) : ?>
+                    <div class="card-header"><?php echo $semArr[$i]['semester'] ?></div>
+                    <ul class="list-group list-group-flush">
+                        <?php for ($c = 0; $c < $semArr[$i]['count']; $c++) : ?>
+                            <?php $courses = query_courses_taken_on_semester($conn, $semArr[$i]['semester']) ?>
+                            <a href="courseDetailPage.php" class="list-group-item list-group-item-action"><?php echo $courses[$c]['dep_title'] . " " . $courses[$c]['c_num'] . " - " . $courses[$c]['c_name'] ?></a>
+                        <?php endfor; ?>
+                    </ul>
+                    <hr>
+                <?php endfor; ?>
+                <!-- Semester Divider -->
+            </div>
+        </div>
+    </div>
+</div>
